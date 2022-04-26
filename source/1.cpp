320d30d9b983d2a3ddd12a6a80a9b9a819b4d7e5
Ackerman <narek.davtyan2002@mail.ru>
2022-04-26 09:09:37 +0600

    goto

--- source/1.cpp
+++ source/1.cpp
@@ -237,7 +237,7 @@ int Oper::getValue(Lexem *left, Lexem *right) {
 		if (left->getLexemtype() == NUM && right->getLexemtype() == NUM) {
 			return static_cast<Number *>(right)->getValue();
 		}
-	}
+	} 
 	if (left->getLexemtype() == VAR && right->getLexemtype() == VAR) 
 		return evalNum(static_cast<Variable *>(left)->getValue(), static_cast<Variable *>(right)->getValue());
 	if (left->getLexemtype() == VAR && right->getLexemtype() == NUM) {
@@ -309,13 +309,34 @@ std::vector<Lexem *> parseLexem(std::string codeline) {
 	return infix;
 }
 
-void print(std::vector<Lexem *> vec) {
+/*void print(std::vector<Lexem *> vec) {
 	for (int i = 0; i < vec.size(); i++) {
 		if ((int)(vec[i]->getLexemtype())) {
 			cout << "oper is: " << OPERTEXT[(int)(((Oper *)vec[i])->getType())] << endl;
 		}
 		else cout << "number is: " << ((Number *)vec[i])->getValue() << endl;
 	}
+}*/
+
+void print(std::vector<Lexem *> vec) {
+	for (int i = 0; i < vec.size(); i++) {
+		if (vec[i]->getLexemtype() == NUM) {
+			cout << "[" << static_cast<Number *>(vec[i])->getValue() << "] ";
+		}
+		if (vec[i]->getLexemtype() == OPER) {
+			Oper *lexemop = static_cast<Oper *>(vec[i]);
+			if (lexemop->getType() == GOTO) {
+				cout << "[goto] ";
+			}
+			if (lexemop->getType() == ASSIGN) {
+				cout << "[=] ";
+			}
+		}
+		if (vec[i]->getLexemtype() == VAR) {
+			cout << "[" << static_cast<Variable *>(vec[i])->getName() <<  "(" << static_cast<Variable *>(vec[i])->getRow() << ")" << "] ";
+		}
+	}
+	cout << endl;
 }
 
 std::vector<Lexem *> buildPoliz(std::vector<Lexem *> infix) {
@@ -323,6 +344,9 @@ std::vector<Lexem *> buildPoliz(std::vector<Lexem *> infix) {
 	std::vector<Lexem *> postfix;
 	std::stack<Oper *> stack;
 	for (i = 0; i < infix.size(); i++) {
+		if (infix[i] == nullptr) {
+			continue;
+		}
 		if (infix[i]->getLexemtype() == NUM) {
 			postfix.push_back(infix[i]);
 		}
@@ -367,7 +391,7 @@ std::vector<Lexem *> buildPoliz(std::vector<Lexem *> infix) {
 			stack.pop();
 		}
 	}
-	print(postfix);
+	//print(postfix);
 	return postfix;
 }
 
@@ -375,12 +399,15 @@ std::vector<Lexem *> buildPoliz(std::vector<Lexem *> infix) {
 int evaluatePoliz(std::vector<Lexem *> postfix, int &row) {
 	std::stack<Lexem *> stack;
 	for (int i = 0; i < postfix.size(); i++) {
+		if (postfix[i] == nullptr) {
+			continue;
+		}
 		if (postfix[i]->getLexemtype() == NUM || postfix[i]->getLexemtype() == VAR) {
 			stack.push(postfix[i]);
 		}
 		else {
 			if (static_cast<Oper *>(postfix[i])->getType() == GOTO) {
-				return static_cast<Variable *>(postfix[i + 1])->getRow();
+				return static_cast<Variable *>(postfix[i - 1])->getRow();
 			}
 			Lexem *right = stack.top();
 			stack.pop();
@@ -391,10 +418,12 @@ int evaluatePoliz(std::vector<Lexem *> postfix, int &row) {
 			stack.push(new Number(n));
 		}
 	}
-	Number *ans = static_cast<Number *>(stack.top());
-	stack.pop();
-	cout <<  "answer is: " << ans->getValue() << endl;
-	return row++;
+	if (stack.size()) { 
+		Number *ans = static_cast<Number *>(stack.top());
+		stack.pop();
+		cout <<  "answer is: " << ans->getValue() << endl;
+	}
+	return row + 1;
 }
 
 void initLabels(std::vector<Lexem *> &infix, int &row) {
@@ -404,13 +433,10 @@ void initLabels(std::vector<Lexem *> &infix, int &row) {
 			Oper *lexemop = static_cast<Oper *>(infix[i]);
 			if (lexemop->getType() == COLON) {
 				labels[lexemvar->getName()] = row;
-				auto elem = infix.begin() + i - 1;
-				infix.erase(elem);
-				infix.erase(elem);
-				// delete infix[i - 1];
-				// delete infix[i];
-				// infix[i - 1] = nullptr;
-				// infix[i] = nullptr;
+				delete infix[i - 1];
+				delete infix[i];
+				infix[i - 1] = nullptr;
+				infix[i] = nullptr;
 				i++;
 			}
 		} 
@@ -428,8 +454,11 @@ int main() {
 		initLabels(infixLines[row], row);
 	}
 	for (const auto &infix: infixLines) {
-		postfixLines.push_back(buildPoliz(infix));
+	 	postfixLines.push_back(buildPoliz(infix));
 	}
+	// for (int i = 0; i < postfixLines.size(); i++) {		
+ // 		print(postfixLines[i]);
+	// }
 	int row = 0;
 	while (0 <= row && row < postfixLines.size()) {
 		row = evaluatePoliz(postfixLines[row], row);
